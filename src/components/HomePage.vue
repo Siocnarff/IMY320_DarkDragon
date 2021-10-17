b5501747e660e38505289011d1dc0c614d028499
Patric Edwards <u18009337@tuks.co.za>
2021-10-17 12:33:11 +0200

    Github links

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,18 +1,26 @@
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
 <template>
+
+
     <ul id="v-for-object" class="demo">
       <li class= "projects" v-for="value in items" :key="value.id">
         <p class="headings"> {{ value.title }}</p>
 
 
         <div class="image">
-          <img class="imgs" :src="value.link" :alt="value.link">
+          <img class="imgs" :src="value.link" :alt="value.link" >
           <!---<img src="../assets/YOUR_JAMS.png"/>--->
-          <div class="overlay">
+          <div class="overlay " >
 
             <div class = "center_text">
               <h1 class="headings_for_overlay">{{ value.info_heading }}</h1>
               <h1 class="catch_phrase">{{value.catch_phrase}}</h1>
               <p class="info_text">{{value.text}}</p>
+              <i class="fa fa-github" style="font-size:48px;color:red"></i>
+              <a :href="value.github_link" >GitHub</a>
             </div>
 
           </div>
@@ -25,16 +33,20 @@
 
  <script>
 
+
  export default {
    name: "HomePage",
+
    el: '#v-for-object',
    data() {
      return {
        items: [
          {title: 'YOUR JAMS', link:  require('../assets/YOUR_JAMS.png'), info_heading:'AI MUSIC RECOMMENDATION', catch_phrase:"Your tunes always lekker"
-         , text:"This app helps you find the music you're into quickly on the go. It learns what you're into and recommends songs you might like, dont worry it learns with you so as your tastes in music changes so will its recommendations.\n"},
+         , text:"This app helps you find the music you're into quickly on the go. It learns what you're into and recommends songs you might like, dont worry it learns with you so as your tastes in music changes so will its recommendations.",
+         github_link: "https://github.com/COS301-SE-2021/Escape-Room-Planner"},
          {title: "FIND YOUR WAY", link: require('../assets/ERP.jpg'), info_heading:'ESCAPE ROOM SIMULATION AND PLANNING',catch_phrase:"Always a way out",
-         text:"Escape Room Planner intends to solve an organisational problem that user’s face when constructing an escape room. The system will allow users to simulate custom escape rooms that can be implemented in the real world"}
+         text:"Escape Room Planner intends to solve an organisational problem that user’s face when constructing an escape room. The system will allow users to simulate custom escape rooms that can be implemented in the real world",
+           github_link: "https://github.com/COS301-SE-2021/Escape-Room-Planner"}
        ]
 
 
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import './assets/reset.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
 
 // Make BootstrapVue available throughout your project

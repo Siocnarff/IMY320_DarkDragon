373f7fc7ffe46f54e42103a84c9af2022a93de28
Patric Edwards <u18009337@tuks.co.za>
2021-10-17 13:57:21 +0200

    Ok this is taking long

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="logo" alt="Vue logo" src="./assets/DDDLogo.png">
+    <div>
+      <Navbar id = "Navb"/>
+    </div>
+    <h1 id="DDDHeading">DARK DRAGON DESIGN</h1>
+    <HomePage id="homepage"/>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+import Navbar from "./components/Navbar";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage,Navbar
   }
 }
+
+
+
 </script>
 
 <style>
+html,
+body,
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+  background-color: #DCE0D9;
+  cursor:url(./assets/CursorImg.png), auto;
 }
+
+#logo{
+  width: 100px;
+  display: inline-flex;
+  position: absolute; left: 100px; top:30px;
+}
+
+#DDDHeading{
+  position: absolute; left: 300px; top:85px;
+  font-size: 50px;
+  font-family: 'Spartan',serif;
+  font-weight: bold;
+}
+
+#Navb{
+  position: absolute; left: 1200px; top:90px;
+  font-family: 'Libre Baskerville',serif;
+}
+
+
+
+
 </style>

06f04bd699ca21c24d5634df1fe7336960a41591
Patric Edwards <u18009337@tuks.co.za>
2021-10-22 16:40:24 +0200

    Working on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="about">
-
+    <div id="top">
+    </div>
     <img id="Diver" src="../assets/DiverForAbout.png">
-    <h1 id="HeadingStyle"><span style="color: #372549;">The </span><span style="color: #DCE0D9;">Only Deep Water Developers </span></h1>
-     <p class="par" id="p1"> With our experience in software development, web design and, get
+    <h1 id="HeadingStyle"><span style="color: #DB162F;">The </span><span style="color: #DCE0D9;">Only Deep Water Developers </span></h1>
+    <p class="par" id="p1"> With our experience in software development, web design and, get
       this - wreck diving, we go wherever it is required to go to ensure your
       software works.</p>
+
+
     <img id="PathFinder" src="../assets/Pf.png">
     <p class="par" id="p2">Where other teams say there is no way we find a way. No one on our team is scared of a little path finding algorithm.</p>
     <h1 id="PFheading">The Path Finders</h1>
+
+
+
   </div>
+
+
 </template>
 
 <style>
@@ -37,9 +45,9 @@
   #PFheading{
     position: relative;
     left: 30%;
-    top: 250px;
+    top: 230px;
     margin-bottom: 600px;
-    color: #372549;
+    color: #DCE0D9;
     font-style: italic;
     font-family: 'Libre Baskerville',serif;
     font-size: 30px;
@@ -70,8 +78,17 @@
   .par{
     width: 400px;
     text-align: left;
-    color: #372549;
+    color: #DCE0D9;
     font-size: 20px;
+  }
+
+  #top{
+    position: absolute;
+    top: 200px;
+    width:100%;
+    height: 850px;
+
+    background-color: #372549;
   }
 
 </style>

7cf583165c5740f7a1e5055d06cd0c7fa4b1ff80
GreenLizzerd <hdp.green.lizzerd.rsa@gmail.com>
2021-10-22 21:45:10 +0200

    added more grid spase

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -6,6 +6,8 @@
     <div class="colims2">
       <img class="item1" src="src\assets\logo_shert_1.png" />
       <img class="item2" src="src\assets\logo_shert_404.png" />
+      <img class="item1p" src="src\assets\add_black_24dp_2.png" />
+      <img class="item2p" src="src\assets\add_black_24dp_2.png" />
     </div>
   </div>
 </template>
@@ -17,24 +19,41 @@ export default {
 </script>
 
 <style scoped>
-.item1{
+.item1 {
   grid-column-start: 1;
   grid-column-end: 2;
+  justify-self: stretch;
 }
-.item1{
+.item2 {
   grid-column-start: 2;
-  grid-column-end:3;
+  grid-column-end: 3;
+  justify-self: stretch;
+}
+.item1p {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  justify-self: end;
+  align-self: end;
+}
+.item2p {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  justify-self: end;
+  align-self: end;
 }
 .storButin {
   justify-self: end;
 }
 .colims1 {
   grid-template-columns: repeat(1, 1fr);
+  justify-content: center;
 }
 .colims2 {
   grid-template-columns: repeat(2, 1fr);
+  justify-content: center;
 }
 .colims3 {
   grid-template-columns: repeat(3, 1fr);
+  justify-content: center;
 }
 </style>

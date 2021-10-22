eaf40aaf3cb9f1d3e6b3739661c4946ddd819208
Patric Edwards <u18009337@tuks.co.za>
2021-10-22 12:55:42 +0200

    Working on about page

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,6 +1,7 @@
 <template>
 
-<div>
+<div id="about">
+
 
 </div>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <img id="Diver" src="../assets/DiverForAbout.png">
+    <h1 id="PFheading">The Path Finders</h1>
   </div>
 </template>
+
+<style>
+  #Diver{
+    position: relative;
+    right: 25%;
+    top: 200px;
+  }
+
+  .about{
+
+  }
+
+  #PFheading{
+    position: relative;
+    left: 40%;
+    top: 200px;
+    margin-bottom: 200px;
+  }
+</style>

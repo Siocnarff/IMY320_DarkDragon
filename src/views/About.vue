93cb232f518d6210e8a99e2e2ac3bf8c04d0733c
Patric Edwards <u18009337@tuks.co.za>
2021-10-22 13:18:35 +0200

    Working on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,29 @@
 <template>
   <div class="about">
+
     <img id="Diver" src="../assets/DiverForAbout.png">
+    <h1 id="HeadingStyle"><span style="color: #372549;">The </span><span style="color: #DCE0D9;">Only Deep Water Developers </span></h1>
+    <img id="PathFinder" src="../assets/Pf.png">
     <h1 id="PFheading">The Path Finders</h1>
   </div>
 </template>
 
 <style>
+
   #Diver{
     position: relative;
-    right: 25%;
-    top: 200px;
+    right:20%;
+    top: 170px;
+    width: 40%;
+    height: 400px;
+
+  }
+
+  #PathFinder{
+    position: absolute;
+    left: 45%;
+    top: 55%;
+    width: 40%;
   }
 
   .about{
@@ -18,8 +32,23 @@
 
   #PFheading{
     position: relative;
-    left: 40%;
-    top: 200px;
-    margin-bottom: 200px;
+    left: 30%;
+    top: 450px;
+    margin-bottom: 600px;
+    color: #372549;
+    font-style: italic;
+    font-family: 'Libre Baskerville',serif;
+    font-size: 30px;
+
   }
+
+  #HeadingStyle{
+    position: absolute;
+    left: 7.5%;
+    top: 52%;
+    font-style: italic;
+    font-family: 'Libre Baskerville',serif;
+    font-size: 30px;
+  }
+
 </style>

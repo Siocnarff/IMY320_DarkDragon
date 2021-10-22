c7b5d33100315202da807594460c502c2a2be42b
GreenLizzerd <hdp.green.lizzerd.rsa@gmail.com>
2021-10-22 20:06:46 +0200

    ferste step no store

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Hello World</div>
+  <div id="stoor">
+    <div class="colims1">
+      <p class="storButin" > </p>
+    </div>
+    <div class="colims2">
+
+
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +17,16 @@ export default {
 </script>
 
 <style scoped>
-
+.storButin{
+  justify-self: end;
+}
+.colims1{
+  grid-template-columns:repeat(1, 1fr);
+}
+.colims2{
+  grid-template-columns:repeat(2, 1fr);
+}
+.colims3{
+  grid-template-columns:repeat(3, 1fr);
+}
 </style>

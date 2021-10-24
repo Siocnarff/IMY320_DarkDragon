03df243ccc382a9b7d3af72298a5c2d67b2e927f
GreenLizzerd <hdp.green.lizzerd.rsa@gmail.com>
2021-10-24 04:30:05 +0200

    Update Merch.vue

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -4,13 +4,27 @@
       <p class="storButin"></p>
     </div>
     <div class="colims2">
-      <div class="blok2x2">
+      <div class="blok3x3">
         <img class="item" src="../assets/logo_shert_1.png" alt="" />
-        <img class="itemp" src="../assets/add_black_24dp_2.png" alt="" />
+        <div class="itemp">
+          <img src="../assets/add_black_24dp_2.png" alt="" />
+        </div>
+        <div class="itemh">
+          <h1>
+            R6000
+          </h1>
+        </div>
       </div>
-      <div class="blok2x2">
+      <div class="blok3x3">
         <img class="item" src="../assets/logo_shert_404.png" alt="" />
-        <img class="itemp" src="../assets/add_black_24dp_2.png" alt="" />
+        <div class="itemp">
+          <img src="../assets/add_black_24dp_2.png" alt="" />
+        </div>
+        <div class="itemh">
+          <h1>
+            R6000
+          </h1>
+        </div>
       </div>
     </div>
   </div>
@@ -23,22 +37,34 @@ export default {
 </script>
 
 <style scoped>
-.blok2x2 {
+.blok3x3 {
   display: grid;
-  grid-template-columns:6fr,1fr ;
-  grid-template-rows:6fr,1fr ;
+  grid-template-columns: 3fr, 1fr, 2fr;
+  grid-template-rows: 3fr, 1fr, 2fr;
 }
 .item {
-  grid-column-start: 1;
-  grid-column-end: 3;
+  grid-area: 1/1/4/4;
   justify-self: stretch;
   align-self: stretch;
 }
 .itemp {
-  grid-column-start: 2;
-  grid-column-end: 3;
+  grid-area: 3/3/4/4;
   align-self: end;
   justify-self: end;
+}
+div.itemh {
+  grid-row-start: 1;
+  grid-column-start: 1;
+  grid-row-end: 4;
+  grid-column-end: 4;
+  opacity: 0;
+  align-self: stretch;
+  justify-self: stretch;
+  background-color: #1A1423;
+  color: rgb(220, 224, 217);
+}
+div.itemh:hover {
+  opacity: 1;
 }
 .storButin {
   justify-self: end;
@@ -50,7 +76,7 @@ export default {
 }
 .colims2 {
   display: grid;
-  grid-template-columns:minmax(40%,100px ) minmax(40%,100px );
+  grid-template-columns: minmax(40%, 100px) minmax(40%, 100px);
   justify-content: center;
 }
 .colims3 {

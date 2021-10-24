c75ddc4b39aac236a9a5874fb7eec5272e3434aa
GreenLizzerd <hdp.green.lizzerd.rsa@gmail.com>
2021-10-24 03:42:18 +0200

    Update Merch.vue
    more reatif

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="stoor">
-    <div class="colims1">
+    <div class="colims2">
       <p class="storButin"></p>
     </div>
     <div class="colims2">
-      <img class="item1" src="../assets/logo_shert_1.png"  alt=""/>
-      <img class="item2" src="../assets/logo_shert_404.png"  alt=""/>
-      <img class="item1p" src="../assets/add_black_24dp_2.png"  alt=""/>
-      <img class="item2p" src="../assets/add_black_24dp_2.png"  alt=""/>
+      <div class="blok2x2">
+        <img class="item" src="../assets/logo_shert_1.png" alt="" />
+        <img class="itemp" src="../assets/add_black_24dp_2.png" alt="" />
+      </div>
+      <div class="blok2x2">
+        <img class="item" src="../assets/logo_shert_404.png" alt="" />
+        <img class="itemp" src="../assets/add_black_24dp_2.png" alt="" />
+      </div>
     </div>
   </div>
 </template>
@@ -19,40 +23,38 @@ export default {
 </script>
 
 <style scoped>
-.item1 {
-  grid-column-start: 1;
-  grid-column-end: 2;
-  justify-self: stretch;
+.blok2x2 {
+  display: grid;
+  grid-template-columns:6fr,1fr ;
+  grid-template-rows:6fr,1fr ;
 }
-.item2 {
-  grid-column-start: 2;
+.item {
+  grid-column-start: 1;
   grid-column-end: 3;
   justify-self: stretch;
+  align-self: stretch;
 }
-.item1p {
-  grid-column-start: 1;
-  grid-column-end: 2;
-  justify-self: end;
-  align-self: end;
-}
-.item2p {
+.itemp {
   grid-column-start: 2;
   grid-column-end: 3;
-  justify-self: end;
   align-self: end;
+  justify-self: end;
 }
 .storButin {
   justify-self: end;
 }
 .colims1 {
+  display: grid;
   grid-template-columns: repeat(1, 1fr);
   justify-content: center;
 }
 .colims2 {
-  grid-template-columns: repeat(2, 1fr);
+  display: grid;
+  grid-template-columns:minmax(40%,100px ) minmax(40%,100px );
   justify-content: center;
 }
 .colims3 {
+  display: grid;
   grid-template-columns: repeat(3, 1fr);
   justify-content: center;
 }

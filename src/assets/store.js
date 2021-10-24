97e0586d9bf03e399bdc708f2310ffab2371fe23
GreenLizzerd <hdp.green.lizzerd.rsa@gmail.com>
2021-10-24 14:03:21 +0200

    Update store.js

--- src/assets/store.js
+++ src/assets/store.js
@@ -9,10 +9,12 @@ function updateCart(){
 document.getElementById("S1").addEventListener("click", function () {
     cart.push("1");
     console.log(cart);
+    updateCart()
 });
 document.getElementById("S1").addEventListener("click", function () {
     cart.push("2");
     console.log(cart);
+    updateCart()
 });
 
 

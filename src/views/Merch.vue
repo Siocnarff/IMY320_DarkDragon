eb9c7243cca984ba6103a16bb12cca7e98fc591b
Patric Edwards <u18009337@tuks.co.za>
2021-10-25 22:05:07 +0200

    Working on merch

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -7,19 +7,29 @@
         <div class="MerchGrid">
 
 
-
+          <div class =ldiv>
             <ul class="right">
 
-              <li>
+              <li class="members">
                 <img   :src="value.link " class="merchImgleft">
+                <div class="loverlay">
+                  <h1 class="heading">{{value.Name}}</h1>
+
+                </div>
               </li>
             </ul>
+          </div>
 
 
         <div class =rdiv>
           <ul>
             <li>
               <img   :src="value.link2 " class="merchImgRight">
+
+              <div class="roverlay">
+                <h1 class="heading2">{{value.Name2}}</h1>
+
+              </div>
             </li>
 
           </ul>
@@ -45,17 +55,17 @@ export default {
       items: [
         {
 
-          Name: 'Shirt2',
+          Name: 'Person Not Found',
           link: require('../assets/Shirt2.png')
-          ,Name2: 'Shirt1',
+          ,Name2: 'DDD-T',
         link2: require('../assets/Shirt1.png')
 
         },
         {
 
-          Name: 'Shirt1',
+          Name: 'DDD-T',
           link: require('../assets/Shirt1.png')
-          ,Name2: 'Shirt2',
+          ,Name2: 'Person Not Found',
           link2: require('../assets/Shirt2.png')
 
         },
@@ -71,6 +81,10 @@ export default {
 
 </script>
 <style scoped>
+
+.members{
+  list-style: none;
+}
 
 #store{
   padding-top: 1%;
@@ -90,6 +104,8 @@ export default {
 
 }
 
+
+
 .merchImgRight{
   margin-top: 5%;
   margin-right: 20%;
@@ -105,5 +121,55 @@ export default {
 .rdiv{
   background-color: #1A1423;
 }
+
+
+.loverlay{
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+}
+
+.roverlay{
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+}
+
+.merchImgRight:hover + .roverlay{
+  opacity: 1;
+}
+
+.merchImgleft:hover + .loverlay{
+  opacity: 1;
+}
+
+.merchImgleft:hover{
+  opacity: 0.3;
+}
+
+.merchImgRight:hover{
+  opacity: 0.3;
+}
+
+.heading {
+  color: #DB162F;
+  font-family: Spartan, serif;
+  font-size: 57px;
+  z-index: 1;
+  position: relative; top: -550px;
+  text-align: left;
+}
+
+.heading2 {
+  color:  #F9C80E;
+
+  font-family: Spartan, serif;
+  font-size: 57px;
+  z-index: 1;
+  position: relative; top: -550px;
+  margin-right: 5%;
+  text-align: right;
+}
+
 
 </style>

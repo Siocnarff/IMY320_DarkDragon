84689990811996d834fcb950d7c319035428b6d1
Patric Edwards <u18009337@tuks.co.za>
2021-10-25 20:29:05 +0200

    Making sure its up to date

--- src/views/Merch.vue
+++ src/views/Merch.vue
@@ -1,31 +1,14 @@
 <template>
-  <div id="stoor">
-    <div class="colims1">
-      <p  class="storButin">Cart</p>
-      <p id="cartnum">
+  <div id="store">
 
-      </p>
-    </div>
-    <div class="colims2">
-      <div class="blok3x3">
-        <img class="item" src="../assets/logo_shert_1.png" alt="" />
-        <div class="itemh">
-          <h1>R6000</h1>
+    <ul id="v-for-members" class="demo" style="list-style: none;">
+      <li class= "members" v-for="value in items" :key="value.id">
+
+        <div class="MerchGrid">
+
         </div>
-        <div  id="S1" class="itemp">
-          <img src="../assets/add_black_24dp_2.png" alt="" />
-        </div>
-      </div>
-      <div class="blok3x3">
-        <img class="item" src="../assets/logo_shert_404.png" alt="" />
-        <div class="itemh">
-          <h1>R6000</h1>
-        </div>
-        <div id="S2" class="itemp">
-          <img src="../assets/add_black_24dp_2.png" alt="" />
-        </div>
-      </div>
-    </div>
+      </li>
+    </ul>
     
   </div>
 </template>
@@ -35,73 +18,27 @@
 // require("..\\assets\\store.js");
 export default {
   name: "Merch.vue",
-
-  mounted() {
-    window.scrollTo(0, 0);
+  el: '#v-for-merch',
+  data() {
+    return {
+      items: [
+        {
+          Name: 'Shirt1',
+          link: require('../assets/Shirt1.png'),
+        }
+      ]
+    }
   }
+
 };
 
 </script>
 <style scoped>
-.blok3x3 {
+
+.MerchGrid{
   display: grid;
-  grid-template-columns: 3fr, 1fr, 2fr;
-  grid-template-rows: 3fr, 1fr, 2fr;
+  grid-template-columns: 1fr 1fr 1fr;
+  padding-bottom: 20px;
 }
-.item {
-  grid-area: 1/1/4/4;
-  justify-self: stretch;
-  align-self: stretch;
-}
-.itemp {
-  grid-area: 3/3/4/4;
-  align-self: end;
-  justify-self: end;
-}
-div.itemh {
-  grid-row-start: 1;
-  grid-column-start: 1;
-  grid-row-end: 4;
-  grid-column-end: 4;
-  opacity: 0;
-  align-self: stretch;
-  justify-self: stretch;
-  background-color: #1a1423;
-  color: rgb(220, 224, 217);
-  border-radius: 50px;
-}
-div.itemh:hover {
-  opacity: 0.75;
-}
-.storButin {
-  font-weight: bold;
-  justify-self: end;
-  color: #1a1423;
-  font-family: "Libre Baskerville", serif;
-  font-style: italic;
-  font-size: 27px;
-}
-.cartnum {
-  font-weight: bold;
-  justify-self: end;
-  color: #a51313;
-  font-family: "Libre Baskerville", serif;
-  font-style: italic;
-  font-size: 27px;
-}
-.colims1 {
-  display: grid;
-  grid-template-columns: 80%;
-  justify-content: center;
-}
-.colims2 {
-  display: grid;
-  grid-template-columns: minmax(40%, 100px) minmax(40%, 100px);
-  justify-content: center;
-}
-.colims3 {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  justify-content: center;
-}
+
 </style>
